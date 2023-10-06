f349be6c7df01c3c03f4f28de4b641c924507521
ademnr <ademnr2@gmail.com>
2023-10-06 17:51:50 +0100

    changing back

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const express = require('express');
+const express = require("express");
+var http = require("http");
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const messages = []
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
 
+//middlewre
+app.use(express.json());
+var clients = {};
 
-io.on('connection', (socket) => {
-  const username = socket.handshake.query.username
-  socket.on('message', (data) => {
-    const message = {
-      message: data.message,
-      senderUsername: username,
-      sentAt: Date.now()
-    }
-    messages.push(message)
-    io.emit('message', message)
-
-  })
+io.on("connection", (socket) => {
+  console.log("connetetd");
+  console.log(socket.id, "has joined");
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
+  });
+  socket.on("message", (msg) => {
+    console.log(msg);
+    let targetId = msg.targetId;
+    if (clients[targetId]) clients[targetId].emit("message", msg);
+  });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(port, "0.0.0.0", () => {
+  console.log("server started");
 });
